87ced49aedb37e949a6bc20c01643a06371aec94
DoppioJP <jakub@doppio.jp>
2013-08-22 17:36:34 +0100

    GitHub issues import script corrected.
    index variable in line 136 wasn't available there so I used the "number" from the source.

--- gh-issues-import.py
+++ gh-issues-import.py
@@ -133,7 +133,7 @@ def import_labels(labels):
 
 def import_issues(issues, dst_milestones, dst_labels):
     for source in issues:
-        print colored("Importing issue %s (%s)" % (source["title"], index), 'green')
+        print colored("Importing issue %s (%s)" % (source["title"], source["number"]), 'green')
         import_issue(source, dst_milestones, dst_labels)
         print ""
         
